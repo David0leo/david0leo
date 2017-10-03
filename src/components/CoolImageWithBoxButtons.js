ab2417e220605a2a7b924226d7b5887b3254704d
David0leo <developing.david@gmail.com>
2017-10-03 13:24:18 -0600

    yuck

--- src/components/CoolImageWithBoxButtons.js
+++ src/components/CoolImageWithBoxButtons.js
@@ -1,24 +1,16 @@
 import React from "react";
-import TopBoxButton from "./TopBoxButton";
-import MiddleBoxButton from "./MiddleBoxButton";
-import BottomBoxButton from "./BottomBoxButton";
+import BoxButtons from "./BoxButtons";
 
 class CoolImageWithBoxButtons extends React.Component {
 	render() {
 		return (
 			<div className="cool-image-with-box-buttons">
 				<div className="float-container">
-					<img src={require("../images/IMG_4175.JPG")} alt="no" />
-          <h1>David Anderson</h1>
+					<img src={require("../images/class_straight_title.png")} alt="no" />
+					{/* <h1>David Anderson</h1> */}
 				</div>
 				<div className="float-container">
-					<TopBoxButton />
-				</div>
-				<div className="float-container">
-					<MiddleBoxButton />
-				</div>
-				<div className="float-container">
-					<BottomBoxButton />
+					<BoxButtons />
 				</div>
 			</div>
 		);
