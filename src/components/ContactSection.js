8d7aab870c9520dc9014bf3f3d2bf0cb767ccda7
David0leo <developing.david@gmail.com>
2017-10-12 19:56:22 -0600

    Lots and lots of tweaking style.

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -6,29 +6,31 @@ class ContactSection extends React.Component {
 		return (
 			<div className="contact-section">
 				<TitleBar title={"Contact"} />
-				<div className="contact-option">
-					<a href="mailto:developing.david@gmail.com">
-						<img src={require("../images/gmail.svg")} alt="Mail" />
-						<div>developing.david@gmail.com</div>
-					</a>
-				</div>
-				<div className="contact-option">
-					<a href="https://github.com/David0leo">
-						<img src={require("../images/github_icon.svg")} alt="Github" />
-						<div>Github</div>
-					</a>
-				</div>
-				<div className="contact-option">
-					<a href="https://www.linkedin.com/in/david0leo/">
-						<img src={require("../images/linkedin.svg")} alt="LinkedIn" />
-						<div>LinkedIn</div>
-					</a>
-				</div>
-				<div className="contact-option">
-					<a href="https://www.instagram.com/david0leo/">
-						<img src={require("../images/instagram.svg")} alt="Instagram" />
-						<div>Instagram</div>
-					</a>
+				<div className="contact-options">
+					<div className="contact-option">
+						<a href="mailto:developing.david@gmail.com">
+							<img src={require("../images/gmail.svg")} alt="Mail" />
+							<div>developing.david@gmail.com</div>
+						</a>
+					</div>
+					<div className="contact-option">
+						<a href="https://github.com/David0leo">
+							<img src={require("../images/github_icon.svg")} alt="Github" />
+							<div>Github</div>
+						</a>
+					</div>
+					<div className="contact-option">
+						<a href="https://www.linkedin.com/in/david0leo/">
+							<img src={require("../images/linkedin.svg")} alt="LinkedIn" />
+							<div>LinkedIn</div>
+						</a>
+					</div>
+					<div className="contact-option">
+						<a href="https://www.instagram.com/david0leo/">
+							<img src={require("../images/instagram.svg")} alt="Instagram" />
+							<div>Instagram</div>
+						</a>
+					</div>
 				</div>
 			</div>
 		);
